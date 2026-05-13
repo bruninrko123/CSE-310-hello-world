0130fcf1c3bb7d7eeb9f42696e8218933d308fde
bruninrko123 <balves@byupathway.edu>
2026-05-13 18:11:39 -0300

    Hello world

---
+++ HelloWorldCSE310/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Welcome to CSE 310!. What's your name");
+
+string? name = Console.ReadLine();
+Console.WriteLine($"Hello, {name}! Welcome to CSE 310!");
